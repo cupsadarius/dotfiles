d00f7a75c9f276d12d2bca7cb5e330ae7376868e
Darius Cupsa <darius@optura.ai>
2025-11-30 17:03:58 +0200

    fixes

--- fish/config.fish
+++ fish/config.fish
@@ -82,3 +82,7 @@ else
     # Default theme
     set_fzf_theme catppuccin
 end
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
